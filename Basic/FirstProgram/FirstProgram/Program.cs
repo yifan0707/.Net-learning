f66eb84b01fb082a101d8ad12652df07c4865212
Yifan Lu <lvan0707@outlook.com>
2019-05-13 16:27:29 +1000

    c# first chapter finished UWP knowledged

---
+++ Basic/FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print line
+            Console.WriteLine("Hello, C# World!");
+
+            //waiting user to press enter
+            Console.ReadLine();
+
+            //FirstProgram is a namespace which restrict class name
+        }
+    }
+}
