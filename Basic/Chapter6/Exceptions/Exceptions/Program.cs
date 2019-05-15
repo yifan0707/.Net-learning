232b92acf9570ff5e3c9698cf48e784d22a6595c
Yifan Lu <lvan0707@outlook.com>
2019-05-15 16:13:54 +1000

    exception added

---
+++ Basic/Chapter6/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*unchecked
+            {
+                int willNotThrow = int.MaxValue + 1;
+                Console.WriteLine("Will not throw and it will be executed");
+            }*/
+            checked
+            {
+                int max = int.MaxValue;
+                int willThrow = max + 1;
+                Console.WriteLine("This will never being executed!");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
