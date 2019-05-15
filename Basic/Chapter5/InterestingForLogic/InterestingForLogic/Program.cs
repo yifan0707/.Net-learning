a8c82b16666e071a0dda357527f0c0e4417e34bb
Yifan Lu <lvan0707@outlook.com>
2019-05-15 14:52:27 +1000

    for loop added

---
+++ Basic/Chapter5/InterestingForLogic/InterestingForLogic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InterestingForLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).MultiConditionFor();
+        }
+
+        void MultiConditionFor()
+        {
+            for (String line = Console.ReadLine(); line != ""; line = Console.ReadLine())
+            {
+                Console.WriteLine("Inputed line is: " + line);
+            }
+
+            //interesting for loop
+            for(int i = 0, j = 10; i <= j; i++, j--)
+            {
+                //.....
+            }
+        }
+    }
+}
